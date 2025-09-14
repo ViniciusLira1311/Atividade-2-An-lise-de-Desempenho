7785009f06e543dd1589866983e066147a77d9f0
Vinicius <viniciuspereiralira@hotmail.com>
2025-09-14 19:59:43 -0300

    Definindo a classe da Lista Encadeada

--- Atv2.py
+++ Atv2.py
@@ -2,3 +2,8 @@ class No:
     def __init__(self, valor):
         self.valor = valor  # Valor armazenado no nó
         self.proximo = None  # Referência para o próximo nó
+
+class ListaEncadeada:
+    def __init__(self):
+        self.cabeca = None  # Primeiro elemento da lista
+        self.tamanho = 0    # Quantidade de elementos na lista
