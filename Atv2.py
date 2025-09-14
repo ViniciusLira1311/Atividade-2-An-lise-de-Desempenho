b522eaa91afa82eeb8ef5577edc7c8233a79dd17
Vinicius <viniciuspereiralira@hotmail.com>
2025-09-14 20:01:00 -0300

    Definindo Metodo para verificar se esta vazia e adicionar elemento no inicio

--- Atv2.py
+++ Atv2.py
@@ -7,3 +7,12 @@ class ListaEncadeada:
     def __init__(self):
         self.cabeca = None  # Primeiro elemento da lista
         self.tamanho = 0    # Quantidade de elementos na lista
+
+    def esta_vazia(self):
+        return self.cabeca is None
+    
+    def adicionar_inicio(self, valor):
+        novo_no = No(valor)    # Cria um novo nó
+        novo_no.proximo = self.cabeca  # Novo nó aponta para a antiga cabeça
+        self.cabeca = novo_no  # Cabeça agora aponta para o novo nó
+        self.tamanho += 1      # Incrementa o tamanho da lista
